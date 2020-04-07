ab6ed4d7240c24939034eb6a7032c22850863d07
c19x <c19x>
2020-04-07 10:47:03 +0100

    Empty project

---
+++ app/src/main/java/org/c19x/gui/MainActivity.java
@@ -0,0 +1,16 @@
+package org.c19x.gui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.c19x.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
